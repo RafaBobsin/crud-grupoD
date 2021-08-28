7d9a1e6bdf5f4076e740d1a47f16aa61b892e12b
MellMatsuda <mellamisam1302@gmail.com>
2021-08-28 15:14:27 -0300

    add crud delete tamo bem

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php $connection = new PDO("mysql:host=localhost;dbname=chupeta", "root", ""); ?>
-
+<?php
+    if(isset($_POST['delete'])||isset($_POST['update'])){
+        header('Refresh: 0.1; url=index.php');
+    }
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,13 +13,14 @@
     <title>Loja de Chupeta</title>
 </head>
 <body>
+    <?php require_once 'process.php' ?>
     <h1>Loja de Chupeta</h1>
     <h4>Página administrativa da Chupetinhas</h4>
 
     <h2>Adicionar chupeta</h2>
     <form action="index.php" method="post">
         <label for="marca">Marca: </label>
-        <input type="text" name="marca">
+        <input type="text" name="marca" required>
         <label for="tamanho">Tamanho: </label>
         <input type="text" name="tamanho">
         <label for="cor">Cor: </label>
@@ -23,9 +28,9 @@
         <label for="estampa">Estampa: </label>
         <input type="text" name="estampa">
         <label for="material">Material: </label>
-        <input type="text" name="material">
+        <input type="text" name="material" required>
         <label for="valor">Valor: </label>
-        <input type="text" name="valor">
+        <input type="text" name="valor" required>
         <input type="submit" value="Enviar"/>
         <?php
             if(isset($_POST['marca'], $_POST['tamanho'], $_POST['cor'], $_POST['estampa'], $_POST['material'], $_POST['valor'], )){
@@ -78,42 +83,19 @@
                         <td>".$row['estampa']."</td>
                         <td>".$row['material']."</td>
                         <td>".$row['valor']."</td>
-                        <td>"."<a href='index.php?update=".$row['id_produto']."'>Editar</a>"."</td>
-                        <td>"."<a href='index.php?delete=".$row['id_produto']."'>Deletar</a>"."</td>
+                        <form method='POST'>
+                            <td>"."<button type='submit' name='update' value='".$row['id_produto']."'>Editar</button>"."</td>
+                            <td>"."<button type='submit' name='delete' value='".$row['id_produto']."'>Deletar</button>"."</td>
+                        </form>
                     </tr>";
                 }
             ?>
             <?php
-                if(isset($_GET['id_produto'])){
-                    $id = $_GET['id_produto'];
-                    $sql = "DELETE FROM `produto` WHERE `id_produto`='$id'";
+                if(isset($_POST['delete'])){
+                    $id = $_POST['delete'];
+                    $sql = "DELETE FROM `produto` WHERE `id_produto` = '{$id}'";
 
                     $result = $connection->query($sql);
-                }
-            ?>
-            <?php
-                if(isset($_POST['update'])){
-                    $marca = $_POST['marca'];
-                    $tamanho = $_POST['tamanho'];
-                    $cor = $_POST['cor'];
-                    $estampa = $_POST['estampa'];
-                    $material = $_POST['material'];
-                    $valor = $_POST['valor'];
-
-                    $sql = "UPDATE 'produto' SET 'marca' = '$marca', 'tamanho' = '$tamanho', 'cor' = '$cor', 'estampa' = '$estampa', 'material' = '$material', 'valor' = '$valor', ";
-
-                    $result = $connection->query($sql);
-
-                    if($result->num_rows > 0){
-                        while($row = $result->fetch_assoc()){
-                            $marca = $row['marca'];
-                            $tamanho = $row['tamanho'];
-                            $cor = $row['cor'];
-                            $estampa = $row['estampa'];
-                            $material = $row['material'];
-                            $valor = $row['valor'];
-                        }
-                    }
                 }
             ?>
         </table>
@@ -125,7 +107,7 @@
 <style>
     table, th, td { border:1px solid black; }
 
-    table{ width: 80vw; }
+    table{ width: 90vw; }
 
-    th{ width: 30vw; }
+    th{ width: 40vw; }
 </style>
